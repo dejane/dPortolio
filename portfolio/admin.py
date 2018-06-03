146c25d39fd9c71e5c8f7e1ea6e9a08068e36089
LAPTOP-TCSCID23\dejan <dejan.luznic@gmail.com>
2018-06-03 12:12:37 +0200

    models change, portfolio model does not have instaces anymore

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Portfolio, PortfolioInstance, Coin, CoinInstance
+from .models import Portfolio, Coin, CoinInstance
 
 admin.site.register(Portfolio)
-admin.site.register(PortfolioInstance)
 admin.site.register(Coin)
 admin.site.register(CoinInstance)
--- portfolio/models.py
+++ portfolio/models.py
@@ -38,26 +38,13 @@ class Portfolio(models.Model):
 
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
+    value = models.CharField(max_length=200)
+
+    coins = models.ManyToManyField(CoinInstance, help_text='Select a coins for this portfolio')
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('portfolio-detail', args=[str(self.id)])
-
-
-
-class PortfolioInstance(models.Model):
-
-        id = models.UUIDField(primary_key=True, default=uuid.uuid4,
-                              help_text="Unique ID for this particular portfolio")
-        portfolio = models.ForeignKey('Portfolio', on_delete=models.SET_NULL, null=True)
-        value = models.CharField(max_length=200)
-
-        coins = models.ManyToManyField(CoinInstance, help_text='Select a coins for this portfolio')
-
-        def __str__(self):
-
-            return '{0} ({1})'.format(self.id, self.portfolio.title)
-
 
